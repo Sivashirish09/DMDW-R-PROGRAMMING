5c48970244db887bd2b8e0305aba908819ff971b
Sivashirish09 <169119288+Sivashirish09@users.noreply.github.com>
2025-02-19 15:55:01 +0530

    Delete exp_8 mobile .R

--- exp_8 mobile .R
+++
@@ -1,8 +0,0 @@
-
-x_values <- c(4, 1, 5, 7, 10, 2, 50, 25, 90, 36)  # Number of mobile phones sold
-y_values <- c(12, 5, 13, 19, 31, 7, 153, 72, 275, 110)  # Money earned
-
-plot(x_values, y_values, main = "Scatter Plot of Mobile Phones Sold vs. Money Earned",
-     xlab = "Number of Mobile Phones Sold", ylab = "Money Earned",
-     col = "blue", pch = 19)
-grid()
